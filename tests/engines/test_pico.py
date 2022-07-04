a318c2da3947d2095ae14d7d30b7a23734296826
Giulio Bottari <giuliobottari@gmail.com>
2022-07-04 14:29:42 -0300

    Skip PicoTTS tests if not on linux

--- tests/engines/test_pico.py
+++ tests/engines/test_pico.py
@@ -1,3 +1,4 @@
+import sys
 import pytest
 from tts_wrapper import PicoClient, PicoTTS
 
@@ -14,6 +15,10 @@ class TestPicoOffline(BaseEngineTest):
 
 
 @pytest.mark.slow
+@pytest.mark.skipif(
+    sys.platform != "linux",
+    reason="Skipping PicoTTS synth because it is linux-only.",
+)
 @pytest.mark.parametrize(
     "formats,tts_cls,client",
     [(PicoTTS.supported_formats(), PicoTTS, create_client)],
